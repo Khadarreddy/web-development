543410ae4877f44c826fb30c6408a540f35bd3a8
Khadar Reddy <142429752+Khadarreddy@users.noreply.github.com>
2025-05-11 11:06:32 +0530

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,32 @@
+   function Home() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // smooth scrolling
+    });
+  }
+  function Skills() {
+    window.scrollTo({
+      top: 630, // pixels from the top
+      behavior: "smooth"
+    });
+  }
+  function Education(){
+    window.scrollTo({
+      top: 1350, 
+      behavior: "smooth"
+    });
+  }
+
+  function Projects(){
+    window.scrollTo({
+      top: 2130, 
+      behavior: "smooth"
+    });
+  }
+
+  function Contact(){
+    window.scrollTo({
+      top: 2900, 
+      behavior: "smooth"
+    });
+  }
